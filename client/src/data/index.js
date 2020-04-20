37bc2d7a1f5602d3ebbc0e57e89781f9ac1097a5
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-04-20 12:26:46 +0200

    data input

---
+++ client/src/data/index.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    id: 1,
+    icon: "⌛",
+    status: "open",
+    title: "Job applications",
+    content: "Apply for web development jobs regularly",
+  },
+  {
+    id: 2,
+    icon: "⌛",
+    status: "open",
+    title: "Travel plan",
+    content: "Buy plane tickets when corona virus is over",
+  },
+  {
+    id: 3,
+    icon: "⌛",
+    status: "open",
+    title: "Crypto investments",
+    content: "Open bitcoin trading account",
+  },
+  {
+    id: 4,
+    icon: "⌛",
+    status: "open",
+    title: "Daily learning",
+    content: "Finish watching Harvard CS50",
+  },
+];
+
+const statuses = [
+  {
+    status: "open",
+    icon: "⌛",
+    color: "#EB5A46",
+  },
+  {
+    status: "in progress",
+    icon: "🚧",
+    color: "#00C2E0",
+  },
+  {
+    status: "in review",
+    icon: "🤔",
+    color: "#C377E0",
+  },
+  {
+    status: "done",
+    icon: "✅",
+    color: "#3981DE",
+  },
+];
+
+export { data, statuses };
